370efbbe5802757361356927a7c514d387de7c95
Ethan Thibault <wickedlystudios@gmail.com>
2018-01-26 14:58:05 -0500

    Unity Project Initial Commit
    - Project with Template folders
    - Singleton GameManager script
    - Matched project settings with Spotlight article
    - Changed GitIgnore to look at whole project

---
+++ Global-Game-Jam-2018/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	/* creates singleton pattern:
+	public = anyone can see it
+	static = anyone can access it */
+	public static GameManager instance = null;
+
+	void Awake(){
+		/* Singleton Shit */
+		if (instance == null){instance = this;}
+		else if (instance != this){Destroy(gameObject);}
+		DontDestroyOnLoad(gameObject);
+	}
+}
