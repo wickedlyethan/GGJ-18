eb13021932055d2c09c49f8b45a68dd668a8c116
ChristopherCornford <ccornford02906@gmail.com>
2018-01-27 00:06:00 -0500

    ConsoleStringsStore

---
+++ Global-Game-Jam-2018/Assets/_Scripts/HackingGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HackingGame : MonoBehaviour {
+
+	public InputField console;
+
+	public string inputLine;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			inputLine = console.text;
+			Debug.Log (inputLine);
+		}
+	}
+}
