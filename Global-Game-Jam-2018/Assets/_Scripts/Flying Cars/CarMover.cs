61a43f4688044bae1dfc6b06ed2757e92ac38dc0
Ethan Thibault <wickedlystudios@gmail.com>
2018-01-27 17:13:24 -0500

    Flying Cars System
    - It's really basic, use the Manager to create spawn points, spawn cars, they get deleted when they fly out of the world.

---
+++ Global-Game-Jam-2018/Assets/_Scripts/Flying Cars/CarMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMover : MonoBehaviour {
+
+	public float speed;
+
+    void Start ()
+    {
+    	GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        // GetComponent<rigidbody>().velocity = transform.forward * speed;
+    }
+}
