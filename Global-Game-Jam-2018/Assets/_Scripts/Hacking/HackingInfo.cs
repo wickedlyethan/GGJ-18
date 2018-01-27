9b6c31edefc5b520561caee0355eee53a05e5775
Ethan Thibault <wickedlystudios@gmail.com>
2018-01-27 00:32:41 -0500

    ScriptableObjects for Hacking Game Info
    - Made scriptable objects for containing the information for hacking minigames. For each "game" we'll create a new container of information, which is where the strings that have to be clicked go.
    - Re-organized scripts.

---
+++ Global-Game-Jam-2018/Assets/_Scripts/Hacking/HackingInfo.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class HackingInfo : ScriptableObject
+{
+	[Tooltip("Which hacking event is this?")
+	public int eventID;
+	[Tooltip("This is where the commands the player has to enter go.")]
+	public string[] PlayerCommands;
+}
---
+++ Global-Game-Jam-2018/Assets/_Scripts/Hacking/MakeScriptableObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MakeScriptableObject {
+    [MenuItem("Assets/Create/Hacking Info")]
+    public static void CreateMyAsset()
+    {
+        HackingInfo asset = ScriptableObject.CreateInstance<HackingInfo>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/NewHackingInfo.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+}
