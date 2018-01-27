996a25e68e6c023897662823739249c1e74189ee
Ethan Thibault <wickedlystudios@gmail.com>
2018-01-27 14:09:49 -0500

    Reticle Raycast Complete
    - Can click on objects tagged "Terminal" or "Poster", feedback to come later

--- Global-Game-Jam-2018/Assets/_Scripts/PlayerCharacter/ReticleRaycast.cs
+++ Global-Game-Jam-2018/Assets/_Scripts/PlayerCharacter/ReticleRaycast.cs
@@ -11,6 +11,8 @@ public class ReticleRaycast : MonoBehaviour {
 public float raycastReach = 3;
 
 public Image AspectRatio;
+private Vector3 AspectRatioOn = new Vector3(1,1,1);
+private Vector3 AspectRatioOff = new Vector3(1,1.35f,1);
 
 /* Private */
 [SerializeField] private bool canClick;
@@ -19,18 +21,33 @@ private GameObject currentObj;
 
 void FixedUpdate() 
 {	
+	// Raycast to see if Player is looking at important object
 	RaycastHit hit = new RaycastHit();
-
 	if (Physics.Raycast(transform.position, transform.forward, out hit, raycastReach)){
 		currentObj = hit.collider.gameObject;
 		if (hit.collider.gameObject.tag == "Terminal" | hit.collider.gameObject.tag == "Poster"){
-			AspectRatio.rectTransform.localScale = new Vector3(1,1,1);
+			AspectRatio.rectTransform.localScale = AspectRatioOn;
 			canClick = true;
 		}
 	}
 	else {
-			AspectRatio.rectTransform.localScale = new Vector3(1,1.35f,1);
+			AspectRatio.rectTransform.localScale = AspectRatioOff;
 			canClick = false;
 		}
+
+	// Actual clicking on object
+	if (canClick == true && Input.GetButtonDown("Fire1")){
+		switch (hit.collider.gameObject.tag){
+			case "Terminal":
+				AspectRatio.rectTransform.localScale = AspectRatioOff;
+				// GameManager.instance.LaunchHack();
+				break;
+			case "Poster":
+				AspectRatio.rectTransform.localScale = AspectRatioOff;
+				// Destroy Poster, tell GameManager so it keeps track of how many Posters you've destroyed
+				break;
+		}
+	}
 }
+
 }
