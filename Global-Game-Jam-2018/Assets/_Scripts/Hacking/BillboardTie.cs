3bff4b0b0b5bf620291eadf3ad5d2e799e8136ad
ChristopherCornford <ccornford02906@gmail.com>
2018-01-28 05:23:16 -0500

    Billboard Tying

---
+++ Global-Game-Jam-2018/Assets/_Scripts/Hacking/BillboardTie.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardTie : MonoBehaviour {
+	
+	public HackingGame hackingGame;
+
+	public GameObject billboard;
+	public Material newTexture;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hackingGame.triggerBillboard) {
+			billboard.GetComponent<MeshRenderer> ().material = newTexture;
+		}
+	}
+}
